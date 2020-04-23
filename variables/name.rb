20a6176683833d4a358bcab220da78b1c6f97a31
Drew Sessler <drew.sessler@gmail.com>
2020-04-23 16:11:03 -0700

    Adding exercise files for Variables chapter

---
+++ variables/name.rb
@@ -0,0 +1,6 @@
+puts "Please provide your first name: "
+first_name = gets.chomp
+puts "Please provide your last name: "
+last_name = gets.chomp
+puts "Hello, " + first_name + " " + last_name + "!"
+# 10.times {puts name}
